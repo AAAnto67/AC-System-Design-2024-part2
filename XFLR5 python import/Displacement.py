bf45f7f83159abe629ebd9d870d88a9c0d5b1f6c
AAAnto67 <apr150805@gmail.com>
2024-11-28 15:24:45 +0100

    Update Displacement.py

--- XFLR5 python import/Displacement.py
+++ XFLR5 python import/Displacement.py
@@ -1,29 +1,30 @@
 from Momentdiagram import *
 import numpy as np
 
+def CalcDisplace(I_root):
+    lamb = 0.316
+    E = 70E9
 
-lamb = 0.316
-E = 70E9
-I_root = 0.0001
-
-def I(y):
-    I = I_root * (1-y*((1-lamb)/(halfspan)))**4 ###################
-    return(I)
-
+    def I(y):
+        I = I_root * (1-y*((1-lamb)/(halfspan)))**4 ###################
+        return(I)
 
 
-angle_list = []
-displacement_list = []
-angle = 0
 
-for M, y in zip(M_list, y_list):
-    angle_list.append(angle)
-    angle += 1/(E*I(y)) * -M * dy
-    
-displacement = 0
-for angle in angle_list:
-    displacement_list.append(displacement)
-    displacement += angle * dy
+    angle_list = []
+    displacement_list = []
+    angle = 0
 
-plt.plot(y_list, displacement_list)
-plt.show()
+    for M, y in zip(M_list, y_list):
+        angle_list.append(angle)
+        angle += 1/(E*I(y)) * -M * dy
+        
+    displacement = 0
+    for angle in angle_list:
+        displacement_list.append(displacement)
+        displacement += angle * dy
+
+    return(displacement_list, V_list)
+
+#plt.plot(y_list, displacement_list)
+#plt.show()
