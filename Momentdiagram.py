983ab606849861cf7918106e3e979286eb969330
AAAnto67 <apr150805@gmail.com>
2024-12-05 14:23:34 +0100

    Update Momentdiagram.py

--- Momentdiagram.py
+++ Momentdiagram.py
@@ -1,45 +1,54 @@
 from XFLR5 import *
 from matplotlib import pyplot as plt
 
-
-halfspan = 14
-samples = 100
-step = halfspan/samples
-rho = 0.5
-V = 120
-Wengine = 1678.3 * 9.81
-yengine = 4.8
-
-Llist = []
-ylist = []
-Mlist = []
-i = 0
-moment = 0
-
-while i <= halfspan:
-    if abs(i - yengine)<step:
-        Llist.append(0.5 * rho * V**2 * c(i) * step * cl(2, i) - Wengine)
-
-    else:
-        Llist.append(0.5 * rho * V**2 * c(i) * step * cl(2, i))
-        
-    ylist.append(i)
-    i += step
-
-k=0
-n=0
-while k <= halfspan:
-    for j in range(len(ylist) - n):
-        moment += ylist[j + n] * Llist[j + n]
-    Mlist.append(moment)
+def moment(weight, V, loadfactor)
+    halfspan = 13
+    dy = 0.1
+    rho = 0.5
+    engine_weight = 1678.3 * 9.81
+    y_engine = 4.8
+    trust = 78500
+    r_engine = 1.7
+    engine_angle = 30
+    volume = 12.47
+    density = weight / volume
+    
+    V_list = []
+    y_list = []
+    M_list = []
+    y = 0
     moment = 0
-    k += step
-    n += 1
-
-plt.plot(ylist, Mlist)
-plt.show()
-
-
+    area = lambda x: (4.18 - 0.207936 * x)**2 * 0.11
+    
+    
+    while y <= halfspan:
+        segment_lift = 0.5 * rho * V**2 * c(y) * cl(0, y)
+        segment_weight = area(y) * density
+    
+        V_list.append((segment_lift - segment_weight) * loadfactor) 
+        y_list.append(y)   
+        y += dy
+        
+    
+    
+    for n in range(len(y_list)):
+        y = y_list[n]  
+        moment = 0
+    
+        for j in range(n, len(y_list)):
+            arm = y_list[j] - y
+            moment += arm * V_list[j] * dy
+    
+        if y < y_engine:
+            moment += loadfactor * (engine_weight * (y_engine - y) - trust * r_engine * np.sin(engine_angle))
+    
+        M_list.append(moment)
+        
+    return(M_list, V_list)
+    
+        
+    #plt.plot(y_list, V_list)
+    #plt.show()
 
 
 
