e1a97b6ab2e9b71a0c7b184ab334147770a94090
AAAnto67 <apr150805@gmail.com>
2024-12-05 14:50:30 +0100

    Update ICalc.py

--- ICalc.py
+++ ICalc.py
@@ -5,9 +5,13 @@ import StiffnessCalculations
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import random
+import Displacement
+import LoadCases
 
 root = Tk()
 root.config(bg="gray")
+
+ThrustToWeight = 0.42
 
 Offset = 50
 RootChord = 4.26
@@ -62,13 +66,15 @@ def FindClosest(List, value):
 #Starting w Moments of Inertia
 
 def CalcTruei(ListOfCoord):
-    TopRectangle = [ListOfCoord[1][0] - ListOfCoord[0][0], min([ListOfCoord[0][1], ListOfCoord[1][1]]), min([ListOfCoord[0][1], ListOfCoord[1][1]])/2] #horizontal length, vertical length, Y centroid
-    TopTriangle = [TopRectangle[0], (max([ListOfCoord[0][1], ListOfCoord[1][1]]) - TopRectangle[1]), (TopRectangle[1] + (max([ListOfCoord[0][1], ListOfCoord[1][1]]) - TopRectangle[1])/3)] #Horizontal  length, vertical length, Y centroid
-    BotRectangle = [ListOfCoord[3][0] - ListOfCoord[2][0], max([ListOfCoord[2][1], ListOfCoord[3][1]]), max([ListOfCoord[2][1], ListOfCoord[3][1]])/2] #horizontal length, vertical length, Y centroid
-    BotTriangle = [BotRectangle[0], (min([ListOfCoord[2][1], ListOfCoord[3][1]]) - BotRectangle[1]), (BotRectangle[1] + (min([ListOfCoord[2][1], ListOfCoord[3][1]]) - BotRectangle[1])/3)] #Horizontal  length, vertical length, Y centroid
+    TopRectangle = [ListOfCoord[1][0] - ListOfCoord[0][0], min([ListOfCoord[0][1], ListOfCoord[1][1]]), min([ListOfCoord[0][1], ListOfCoord[1][1]])/2, (ListOfCoord[1][0] - ListOfCoord[0][0])/2] #horizontal length, vertical length, Y centroid, X Centroid
+    TopTriangle = [TopRectangle[0], (max([ListOfCoord[0][1], ListOfCoord[1][1]]) - TopRectangle[1]), (TopRectangle[1] + (max([ListOfCoord[0][1], ListOfCoord[1][1]]) - TopRectangle[1])/3), TopRectangle[0] * 2/3] #Horizontal  length, vertical length, Y centroid, X centroid
+    BotRectangle = [ListOfCoord[3][0] - ListOfCoord[2][0], max([ListOfCoord[2][1], ListOfCoord[3][1]]), max([ListOfCoord[2][1], ListOfCoord[3][1]])/2, (ListOfCoord[3][0] - ListOfCoord[2][0])/2] #horizontal length, vertical length, Y centroid, X centroid
+    BotTriangle = [BotRectangle[0], (min([ListOfCoord[2][1], ListOfCoord[3][1]]) - BotRectangle[1]), (BotRectangle[1] + (min([ListOfCoord[2][1], ListOfCoord[3][1]]) - BotRectangle[1])/3), BotRectangle[0] * 2/3] #Horizontal  length, vertical length, Y centroid, X centroid
     #IMPORTANT if the top or Bot rectangles are both ABOVE or BELOW the chord line the centroid WILL BE WRONGG!!!
 
     FinalCentroid = (TopRectangle[0]*TopRectangle[1]*TopRectangle[2] + TopTriangle[0]*TopTriangle[1]*TopTriangle[2] + BotRectangle[0]*BotRectangle[1]*BotRectangle[2] + BotTriangle[0]*BotTriangle[1]*BotTriangle[2])/(TopRectangle[0]*TopRectangle[1] + TopTriangle[0]*TopTriangle[1] + BotRectangle[0]*BotRectangle[1] + BotTriangle[0]*BotTriangle[1])
+
+    FinalYCentroid = ()
 
     ITopRectangle = (TopRectangle[0]*TopRectangle[1]**3)/12 + TopRectangle[0]*TopRectangle[1]*(TopRectangle[2] - FinalCentroid)**2
     ITopTriangle = (TopTriangle[0]*TopTriangle[1]**3)/36 + 0.5*TopTriangle[0]*TopTriangle[1]*(TopTriangle[2] - FinalCentroid)**2
@@ -78,7 +84,6 @@ def CalcTruei(ListOfCoord):
     return((ITopRectangle + ITopTriangle + IBotRectangle + IBotTriangle), (FinalCentroid))
 
 def CalcString(StringX, ListOfSecondCoord, Angle, Stringheight):
-    #print(StringX * m.tan(Angle))
     StringY = StringX * m.tan(Angle) + Stringheight
     #C.create_line(ScalingFactor*(StringX + ListOfSecondCoord[0][0])/RootChord + Offset, CanvasDimensions[1], ScalingFactor*(StringX + ListOfSecondCoord[0][0])/RootChord + Offset, -ScalingFactor*(StringY)/RootChord + CanvasDimensions[1]*0.5)
     return(StringY)
@@ -115,34 +120,84 @@ def CalcI(FrontSparX, BackSparX, HThickness, VThickness, StringNumTop, StringNum
                     ListOfSecondCoord[3][0]*ScalingFactor/RootChord + Offset, -ListOfSecondCoord[3][1]*ScalingFactor/RootChord + CanvasDimensions[1]*0.5)
     C.coords(FinalYCentroid, -10 + CanvasDimensions[0]*0.5, - FinalCentroid*100 + CanvasDimensions[1]*0.5, 
                                       10 + CanvasDimensions[0]*0.5, -FinalCentroid*100 + CanvasDimensions[1]*0.5)
+                                      
     FinalI = CalcTruei(ListOfCoord)[0] - CalcTruei(ListOfSecondCoord)[0] + StringersTotalI
 
     Ixx.delete("1.0", END)
     Ixx.insert(END, FinalI)
 
-    TorqueList = StiffnessCalculations.deformation('a.txt',86.10,0.3, 78500,resolutionTom,FrontSparX/RootChord,BackSparX/RootChord,HThickness,VThickness) #data, velocity, density, engine thrust, resolution, frontspar (ratio), backspar (ratio), spar thickness, top thickness
-    DeformList = []
-    
-    XList = [round(resolutionTom * i, 3) for i in range(len(TorqueList[0]))]
-    
     ax.clear()
-    ax.plot(XList, TorqueList[0])
-    TomsCanvas.draw()
-
     ax2.clear()
-    ax2.plot(XList, TorqueList[1])
-    TomsCanvas2.draw()
+    ax3.clear()
+    ax4.clear()
 
-    #ax3.clear()
-    #ax3.plot(XList, DeformList[0])
-    #ShearCanvas.draw()
+    LargestTorque = 0
+    LargestBending = 0
+    TorqueCrit = 0
+    BendCrit = 0
 
-    #ax4.clear()
-    #ax4.plot(XList, DeformList[1])
-    #DeformCanvas.draw()
+    """"
+    for i in range(len(LoadCases.WeightsList)):
+        TorqueList = StiffnessCalculations.deformation('a.txt',LoadCases.LoadFactorsList[i], LoadCases.SpeedFactorsList[i],LoadCases.DensitiesList[i], LoadCases.WeightsList[i] * ThrustToWeight,resolutionTom,FrontSparX/RootChord,BackSparX/RootChord,HThickness,VThickness) #data, velocity, density, engine thrust, resolution, frontspar (ratio), backspar (ratio), spar thickness, top thickness
+        DeformList = Displacement.CalcDisplace(FinalI, LoadCases.WeightsList[i], LoadCases.SpeedFactorsList[i], LoadCases.DensitiesList[i], LoadCases.AoAList[i]) #, LoadCases.LoadFactorsList[i]
+        XList = [round(resolutionTom * i, 3) for i in range(len(TorqueList[0]))], [HalfWingSpan * i/len(DeformList[0]) for i in range(len(DeformList[0]))]
+
+        ax.plot(XList[0], TorqueList[0])
+        ax2.plot(XList[0], TorqueList[1])
+        ax3.plot(XList[1], DeformList[1])
+        ax4.plot(XList[1], DeformList[0])
+
+        print(i)
+
+        if TorqueList[1][-1] < LargestTorque:
+            LargestTorque = TorqueList[1][-1]
+            TorqueCrit = i + 1
+        if DeformList[0][-1] < LargestBending:
+            LargestBending = DeformList[0][-1]
+            BendCrit = i + 1
+
+    print(TorqueCrit, LargestTorque)
+    print(BendCrit, LargestBending)
 
     TorqueDeform.delete("1.0", END)
-    TorqueDeform.insert(END, TorqueList[2])
+    TorqueDeform.insert(END, LargestTorque)
+
+    DeflectionDeform.delete("1.0", END)
+    DeflectionDeform.insert(END, LargestBending)
+    """
+
+    
+    TorqueList = StiffnessCalculations.deformation('a.txt',LoadCases.LoadFactorsList[9],LoadCases.SpeedFactorsList[9],LoadCases.DensitiesList[9], LoadCases.WeightsList[9] * ThrustToWeight,resolutionTom,FrontSparX/RootChord,BackSparX/RootChord,HThickness,VThickness) #data, velocity, density, engine thrust, resolution, frontspar (ratio), backspar (ratio), spar thickness, top thickness
+    DeformList = Displacement.CalcDisplace(FinalI, LoadCases.WeightsList[22], LoadCases.SpeedFactorsList[22], LoadCases.DensitiesList[22], LoadCases.AoAList[22], LoadCases.LoadFactorsList[22])
+    XList = [round(resolutionTom * i, 3) for i in range(len(TorqueList[0]))], [HalfWingSpan * i/len(DeformList[0]) for i in range(len(DeformList[0]))]
+    
+
+    ax.plot(XList[0], TorqueList[0])
+    plt.xlabel("funnylabel")
+    plt.ylabel("funnylabel2")
+    TomsCanvas.draw()
+
+    ax2.plot(XList[0], TorqueList[1])
+    plt.xlabel("funnylabel3")
+    plt.ylabel("funnylabel23")
+    TomsCanvas2.draw()
+
+
+    ax3.plot(XList[1], DeformList[1])
+    ax4.plot(XList[1], DeformList[0])
+
+
+    TorqueDeform.delete("1.0", END)
+    TorqueDeform.insert(END, TorqueList[1][-1])
+
+    DeflectionDeform.delete("1.0", END)
+    DeflectionDeform.insert(END, DeformList[0][-1])
+
+    ShearCanvas.draw()
+    DeformCanvas.draw()
+
+
+
 
 FinalI = 0
 C = Canvas(root, bg="white", height=CanvasDimensions[1], width=CanvasDimensions[0])
@@ -158,7 +213,6 @@ for i in range(0, len(FinalAirfoilPoints[0]) - 1):
 #axis
 line = C.create_line(0, CanvasDimensions[1]*0.5, CanvasDimensions[0], CanvasDimensions[1]*0.5, fill='blue')
 line = C.create_line(Offset, 0, Offset, CanvasDimensions[1], fill='blue')
-
 
 
 
@@ -192,12 +246,12 @@ BackSparXINPUT.grid(row = 3, column = 1, sticky = W, padx = 2)
 
 Label(VariableHolder, text="Horizontal thickness (mm)", bg="white", font = 2).grid(row = 4, column = 0, sticky = W, padx = 2)
 HThicknessINPUT = Entry(VariableHolder, bd=3, font=2)
-HThicknessINPUT.insert(0, 2)
+HThicknessINPUT.insert(0, 10)
 HThicknessINPUT.grid(row = 4, column = 1, sticky = W, padx = 2)
 
 Label(VariableHolder, text="Vertical thickness (mm)", bg="white", font = 2).grid(row = 5, column = 0, sticky = W, padx = 2)
 VThicknessINPUT = Entry(VariableHolder, bd=3, font=2)
-VThicknessINPUT.insert(0, 2)
+VThicknessINPUT.insert(0, 10)
 VThicknessINPUT.grid(row = 5, column = 1, sticky = W, padx = 2)
 
 Label(VariableHolder, text="Number of Top Stringers", bg="white", font = 2).grid(row = 6, column = 0, sticky = W, padx = 2)
@@ -223,8 +277,12 @@ Label(OutputHolder, text="Ixx", bg="white", font=2).grid(row = 0, column = 0, st
 Ixx.grid(row=0, column=1, sticky=W)
 
 TorqueDeform = Text(OutputHolder, height = 0.5, width = 10, bg = "light cyan", font =2 , padx=10, pady=5)
-Label(OutputHolder, text="Tom's Deformation in Degrees", bg="white", font=2).grid(row = 1, column = 0, sticky = W, padx = 0)
+Label(OutputHolder, text="Torque in Degrees", bg="white", font=2).grid(row = 1, column = 0, sticky = W, padx = 0)
 TorqueDeform.grid(row=1, column=1, sticky=W)
+
+DeflectionDeform = Text(OutputHolder, height = 0.5, width = 10, bg = "light cyan", font =2 , padx=10, pady=5)
+Label(OutputHolder, text="Downwards Deflection", bg="white", font=2).grid(row = 2, column = 0, sticky = W, padx = 0)
+DeflectionDeform.grid(row=2, column=1, sticky=W)
 
 fig, ax = plt.subplots()
 TomsCanvas = FigureCanvasTkAgg(fig, master=BottomHolder)
@@ -247,6 +305,9 @@ VariableHolder.grid(row = 0, column = 0, sticky = W, padx = 0)
 OutputHolder.grid(row = 1, column = 0, sticky = W, padx = 0)
 BottomHolder.grid(row = 1, column = 0, sticky = W, padx = 0)
 GraphsHolder.grid(row = 2, column  =0, padx = 20, pady = 50)
+
+
+root.mainloop()
 
 
 root.mainloop()
