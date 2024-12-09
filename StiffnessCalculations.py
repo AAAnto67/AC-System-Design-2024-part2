36a7ddb21b680e7f0a2ae05a567bec5264b0dc65
TPdenhaag <52247884+TPdenhaag@users.noreply.github.com>
2024-12-09 17:57:51 +0100

    Update StiffnessCalculations.py

--- StiffnessCalculations.py
+++ StiffnessCalculations.py
@@ -15,7 +15,8 @@ span = 27.5
 #the engine will create a point torque on the wing
 engine_angle = ma.radians(24) 
 engine_hor_dist = 4.8
-engine_vert_dist = 1.7
+engine_x_ratio = 0.1 # this is the chordwise position of the engine. 
+engine_y = 1.7
 engine_weight = 16461.2 
 
 #chord length is defined as c(y) = ay + b, with a = chord_a, and b = chord_b
@@ -23,10 +24,13 @@ engine_weight = 16461.2
 chord_a = -2 * (1 - taper_ratio) * root_chord / span
 chord_b = root_chord
 
-def TorsionalConstant(front_spar_location,rear_spar_location,spar_thickness,top_thickness,y):    
+def TorsionalConstant(front_spar_location,rear_spar_location,spar_t,skin_t,y):    
 
     chord = chord_a*y + chord_b
     delta_spar_location = rear_spar_location - front_spar_location
+
+    spar_thickness = spar_t * chord / root_chord
+    top_thickness = skin_t * chord / root_chord
 
     #the spar height is found using a separate function.
     front_spar_height = af.get_thickness(front_spar_location) * chord
@@ -40,7 +44,7 @@ def TorsionalConstant(front_spar_location,rear_spar_location,spar_thickness,top_
     J = 4 * enclosed_area**2 / line_integral
     return(J)
 
-def Torsion(data,load_factor,velocity,density,engine_thrust,resolution,front_spar_location,rear_spar_location):
+def Torsion(data,load_factor,velocity,density,engine_thrust,resolution,centroid_x,centroid_y):
     
     #the torsion will be calculated in small quantaties based on the moment coefficient in the given location.
     #the torsion of all the parts will then be added together.
@@ -57,20 +61,22 @@ def Torsion(data,load_factor,velocity,density,engine_thrust,resolution,front_spa
         LocalTorsion = 0
         Lift_T = 0
         Moment_T = 0
+
+        engine_chord = (chord_a*engine_hor_dist + chord_b)
         
         #engine contribution to the torsion
         if i <= engine_hor_dist:
-            LocalTorsion += engine_thrust * engine_vert_dist * ma.cos(engine_angle)
+            LocalTorsion += engine_thrust * (engine_y + centroid_y / root_chord * engine_chord) * ma.cos(engine_angle)
 
         if i <= engine_hor_dist:
-            LocalTorsion += -1 * front_spar_location * (chord_a*engine_hor_dist + chord_b) * engine_weight
+            LocalTorsion += -1 * (centroid_x / root_chord - engine_x_ratio) * engine_chord * engine_weight / load_factor
         
         #lift contribution to the torsion
         k = i
         while k < span/2:
             cl = cly(k+resolution/2)
             chord = chord_a*k + chord_b
-            shear_centre_location = (rear_spar_location + front_spar_location) / 2 * chord
+            shear_centre_location = (centroid_x / root_chord * chord)
             load_location = 0.25 * chord
             S = chord * resolution
             L = cl*0.5*density*velocity**2*S
@@ -102,10 +108,10 @@ def Torsion(data,load_factor,velocity,density,engine_thrust,resolution,front_spa
 #xtab = Torsion('a.txt',86.10,0.3,78500,0.1)[0]
 #ytab = Torsion('a.txt',86.10,0.3,78500,0.1)[1]
 
-def deformation(data,load_factor,velocity,density,engine_thrust,resolution,front_spar_location,rear_spar_location,spar_thickness,top_thickness):
+def deformation(data,load_factor,velocity,density,engine_thrust,resolution,front_spar_location,rear_spar_location,spar_thickness,top_thickness,centroid_x,centroid_y):
     G = 28 * 10**9
 
-    torsionf = Torsion(data,load_factor,velocity,density,engine_thrust,resolution,front_spar_location,rear_spar_location)
+    torsionf = Torsion(data,load_factor,velocity,density,engine_thrust,resolution,centroid_x,centroid_y)
     ytab = torsionf[0]
     torsion = torsionf[1]
 
@@ -127,6 +133,7 @@ def deformation(data,load_factor,velocity,density,engine_thrust,resolution,front
     tipdeformation = totaldeformation[-1] 
 
     return(diffdeformation,totaldeformation,tipdeformation)
+
 
 
 
